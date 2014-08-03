0b01b6003374b739cc04cac48370ee0b52c55442
nimrod borochov <nimrodborochov@gmail.com>
2014-08-03 11:22:12 +0300

    Initial Commit

---
+++ BeautyApp/BAPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BAPAppDelegate.h
+//  BeautyApp
+//
+//  Created by nimrod borochov on 8/3/14.
+//  Copyright (c) 2014 pictureit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BAPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BeautyApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BeautyApp
+//
+//  Created by nimrod borochov on 8/3/14.
+//  Copyright (c) 2014 pictureit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BAPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BAPAppDelegate class]));
+    }
+}
