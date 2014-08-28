a236e73ac3a3e9da7cac2447b2b9a2cac42ba948
Nimrod Borochov <nimrodborochov@gmail.com>
2014-08-28 16:51:10 +0300

    add BAPVerificationVC

---
+++ BeautyApp/BAPVerificationVC.h
@@ -0,0 +1,13 @@
+//
+//  BAPVerificationVC.h
+//  BeautyApp
+//
+//  Created by nimrod borochov on 8/28/14.
+//  Copyright (c) 2014 pictureit. All rights reserved.
+//
+
+#import "BAPBaseVC.h"
+
+@interface BAPVerificationVC : BAPBaseVC
+
+@end
---
+++ BeautyApp/BAPVerificationVC.m
@@ -0,0 +1,49 @@
+//
+//  BAPVerificationVC.m
+//  BeautyApp
+//
+//  Created by nimrod borochov on 8/28/14.
+//  Copyright (c) 2014 pictureit. All rights reserved.
+//
+
+#import "BAPVerificationVC.h"
+
+@interface BAPVerificationVC ()
+
+@end
+
+@implementation BAPVerificationVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
